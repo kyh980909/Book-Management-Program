ec92037115e732b10261ec2b9fd9613fe12a731c
kyh980909 <kyh980909@naver.com>
2018-05-03 13:24:46 +0900

    폼 추가

--- BookManager/BookManager/Form1.cs
+++ BookManager/BookManager/Form1.cs
@@ -15,6 +15,18 @@ namespace BookManager
         public Form1()
         {
             InitializeComponent();
+            Text = "도서관 관리";
+
+            label5.Text = DataManager.Books.Count.ToString();
+            label6.Text = DataManager.Users.Count.ToString();
+            label7.Text = DataManager.Books.Where((x) => x.isBorrowed).Count().ToString();
+            label8.Text = DataManager.Books.Where((x) => {
+                return x.isBorrowed && x.BorrowedAt.AddDays(7) < DateTime.Now;
+            }).Count().ToString();
+
+            dataGridView1.DataSource = DataManager.Books;
+            dataGridView2.DataSource = DataManager.Users;
+            dataGridView1.CurrentCellChanged += DataGridView1_CurrentCellChanged;
         }
     }
 }
